fa4d6b1db1c3da9fde2a822910567df4951376dc
Kenny Whyte <kawhyte@gmail.com>
2021-02-18 13:30:33 -0800

    hero

--- components/Hero.js
+++ components/Hero.js
@@ -15,20 +15,25 @@ export default function Landing() {
 							backgroundImage:
 								"url('https://res.cloudinary.com/babyhulk/image/upload/v1613672195/GameBox/pexels-stas-knop-1462725.jpg')",
 						}}>
-						<span id='blackOverlay'className='w-full h-full absolute opacity-75 bg-blac bg-fade2 '></span>  
+						<span
+							id='blackOverlay'
+							className='w-full h-full absolute opacity-75 bg-blac bg-fade2 '></span>
 					</div>
 					<div className='container relative mx-auto '>
 						<div className='items-center flex flex-wrap'>
-							<div className='w-full lg:w-6/12 px-4 ml-auto mr-auto text-center'>
+							<div className='w-full lg:w-8/12 px-4 ml-auto mr-auto text-center'>
 								<div className='pr-12'>
-									<h1 className='text-white font-semibold text-5xl'>
-										Your story starts with us.
-									</h1>
-									<p className='mt-4 text-lg text-gray-300'>
-										This is a simple example of a Landing Page you can build
-										using Tailwind Starter Kit. It features multiple CSS
-										components based on the Tailwindcss design system.
-									</p>
+									<h2 className='text-2xl mb-1  leading-10 font-extrabold text-gray-900 md:sm:text-3xl sm:text-3xl sm:leading-none md:text-4xl'>
+										<p className='text-white p-1'>
+											Track Video Games you’ve played.
+										</p>
+										<p className='text-white p-1'>Save those you want to play.</p>
+										<p className='text-white p-1'>Tell your friends what’s good.</p>
+										<br className='' />
+									</h2>
+                                    <button class="uppercase tracking-widest px-8 py-3 rounded bg-indigo-600 text-white max-w-max shadow-sm hover:shadow-lg">Get Started  - Its Free!</button>
+                                    
+
 								</div>
 							</div>
 						</div>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Trending from "../components/Trending";
 
 export const getStaticProps = async () => {
 	// const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -16,78 +17,18 @@ export const getStaticProps = async () => {
 };
 
 const Home = ({ games }) => {
-
-  console.log(games)
+	//console.log(games)
 	return (
 		<>
 			<Head>
 				<title>Create Next App</title>
 				<link rel='icon' href='/favicon.ico' />
+        
 			</Head>
 
-			<Hero></Hero>
+			<Hero />
 
-
-			<main class='grid  min-h-screen mt-12  p-5 container mx-auto pb-6  flex  content-start items-center justify-center'>
-				<div>
-					<section class='grid grid-cols-1 sm:grid-cols-4 gap-4'>
-						{games.map((game) => (
-							<Link href={`/zelda/${game.id}`} key={game.id}>
-								<div class='bg-gray-900 shadow-lg rounded p-3 max-w-xs'>
-									<div class='group relative'>
-										<img
-											class='w-full md:w-72 block rounded'
-											src={game.img}
-											alt=''
-										/>
-										<div class='absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly'>
-											<button class='hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition'>
-												<svg
-													xmlns='http://www.w3.org/2000/svg'
-													width='20'
-													height='20'
-													fill='currentColor'
-													class='bi bi-heart'
-													viewBox='0 0 16 16'>
-													<path d='M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z' />
-												</svg>
-											</button>
-
-											<button class='hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition'>
-												<svg
-													xmlns='http://www.w3.org/2000/svg'
-													width='40'
-													height='40'
-													fill='currentColor'
-													class='bi bi-play-circle-fill'
-													viewBox='0 0 16 16'>
-													<path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z' />
-												</svg>
-											</button>
-
-											<button class='hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition'>
-												<svg
-													xmlns='http://www.w3.org/2000/svg'
-													width='20'
-													height='20'
-													fill='currentColor'
-													class='bi bi-three-dots'
-													viewBox='0 0 16 16'>
-													<path d='M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z' />
-												</svg>
-											</button>
-										</div>
-									</div>
-									<div class='px-2 py-2 '>
-										<h3 class='text-white text-lg'>{game.title}</h3>
-										<p class='text-gray-400'>{game.genre}</p>
-									</div>
-								</div>
-							</Link>
-						))}
-					</section>
-				</div>
-			</main>
+			<Trending games={games.slice(0, 6)} />
 		</>
 	);
 };
