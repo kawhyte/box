2ea482077f16ca6034192db2ef33234fd5e82a3b
Kenny Whyte <kawhyte@gmail.com>
2021-02-26 19:43:57 -0800

    nav test

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
 import Hero from "./Hero";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar2";
 
 const Layout = ({ children }) => {
 	return (
 		<div className='content'>
-			<Navbar />
+			<Navbar transparent/>
 			{children}
 			<Footer />
 		</div>
