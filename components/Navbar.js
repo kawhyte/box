0d358e7e6110937e459a41db675b994fe35bdc86
Kenny Whyte <kawhyte@gmail.com>
2021-03-02 11:19:40 -0800

    updated Nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,126 +1,179 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 
-const Navbar = () => {
-	return (
-		<nav class=' z-40'>
-			<div class='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
-				<div class='relative flex items-center justify-between h-16'>
-					<div class='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-						<button
-							class='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
-							aria-expanded='false'>
-							<span class='sr-only'>Open main menu</span>
-							<svg
-								class='block h-6 w-6'
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke='currentColor'
-								aria-hidden='true'>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									stroke-width='2'
-									d='M4 6h16M4 12h16M4 18h16'
-								/>
-							</svg>
+export default function Navbar(props) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav
+        className={
+          (props.transparent
+            ? "top-0 absolute z-50 w-full"
+            : "relative  bg-white shadow-lg") +
+          " flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
+        }
+      >
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className={
+                (props.transparent ? "text-white" : "text-gray-800") +
+                " text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
+              }
+              href=""
+            >
+            
 
-							<svg
-								class='hidden h-6 w-6'
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke='currentColor'
-								aria-hidden='true'>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									stroke-width='2'
-									d='M6 18L18 6M6 6l12 12'
-								/>
-							</svg>
-						</button>
-					</div>
-					<div class='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
-					
-					<div class='flex-shrink-0 flex items-center'>
-							<img
-								class='block lg:hidden h-8 w-auto'
-								src='https://res.cloudinary.com/babyhulk/image/upload/v1613758689/GameBox/gamebox-mark-indigo-500-01.svg'
-								alt='Workflow'
-							/>
-							<img
-								class='hidden lg:block h-8 w-auto'
-								src='https://res.cloudinary.com/babyhulk/image/upload/v1613758687/GameBox/gamebox-logo-indigo-500-mark-white-text-01.svg'
-								alt='Workflow'
-							/>
-						</div>
-						
-						<div class='hidden sm:block sm:ml-6 '>
-							<div class='flex space-x-4 uppercase tracking-widest'>
-								<h2 class='text-white px-3 py-2  text-xs'>Best Video Games of:</h2>
+              <img
+              class='hidden lg:block h-8 w-auto'
+              src='https://res.cloudinary.com/babyhulk/image/upload/v1613758687/GameBox/gamebox-logo-indigo-500-mark-white-text-01.svg'
+              alt='Workflow'
+            />
 
-								<a href='#' class='bg-gray-800 text-white px-3 py-2  text-xs'>
-									2020
-								</a>
-								<a href='#' class='bg-gray-800 text-white px-3 py-2  text-xs'>
-									2019
-								</a>
-								<a href='#' class='bg-gray-800 text-white px-3 py-2  text-xs'>
-									2018
-								</a>
-							</div>
-						</div>
-					</div>
-					<div class='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-						<div class='ml-3 relative'>
-							<div>
-								<button
-									class='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
-									id='user-menu'
-									aria-haspopup='true'>
-									<span class='sr-only'>Open user menu</span>
+            </a>
 
-									<img
-										class='h-8 w-8 rounded-full'
-										src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-										alt=''
-									/>
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+            <div class='flex-shrink-0 flex items-start'>
+            <img
+              class='hidden  lg:hidden h-8 w-auto'
+              src='https://res.cloudinary.com/babyhulk/image/upload/v1613758689/GameBox/gamebox-mark-indigo-500-01.svg'
+              alt='Workflow'
+            />
+      
+          </div>
 
-			<div class='hidden sm:hidden'>
-				<div class='px-2 pt-2 pb-3 space-y-1'>
-					<a
-						href='#'
-						class='bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'>
-						Dashboard
-					</a>
-					<a
-						href='#'
-						class='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-						Team
-					</a>
-					<a
-						href='#'
-						class='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-						Projects
-					</a>
-					<a
-						href='#'
-						class='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-						Calendar
-					</a>
-				</div>
-			</div>
-		</nav>
-	);
-};
 
-export default Navbar;
+
+
+
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i
+                className={
+                  (props.transparent ? "text-white" : "text-gray-800") +
+                  " fas fa-bars"
+                }
+              ></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none" +
+              (navbarOpen ? " block rounded shadow-lg" : " hidden")
+            }
+            id="example-navbar-warning"
+          >
+            <ul className="flex flex-col lg:flex-row list-none mr-auto">
+              <li className="flex items-center">
+                <a
+                  className={
+                    (props.transparent
+                      ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                      : "text-gray-800 hover:text-gray-600") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/landing"
+                >
+                  <i
+                    className={
+                      (props.transparent
+                        ? "lg:text-gray-300 text-gray-500"
+                        : "text-gray-500") +
+                      " far fa-file-alt text-lg leading-lg mr-2"
+                    }
+                  />{" "}
+                  About
+                </a>
+              </li>
+            </ul>
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="flex items-center">
+                <a
+                  className={
+                    (props.transparent
+                      ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                      : "text-gray-800 hover:text-gray-600") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href="#pablo"
+                >
+                  <i
+                    className={
+                      (props.transparent
+                        ? "lg:text-gray-300 text-gray-500"
+                        : "text-gray-500") +
+                      " fab fa-facebook text-lg leading-lg "
+                    }
+                  />
+                  <span className="lg:hidden inline-block ml-2">Share</span>
+                </a>
+              </li>
+
+
+
+              <li className="flex items-center">
+                <a
+                  className={
+                    (props.transparent
+                      ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                      : "text-gray-800 hover:text-gray-600") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href="#pablo"
+                >
+                  <i
+                    className={
+                      (props.transparent
+                        ? "lg:text-gray-300 text-gray-500"
+                        : "text-gray-500") +
+                      " fab fa-twitter text-lg leading-lg "
+                    }
+                  />
+                  <span className="lg:hidden inline-block ml-2">Tweet</span>
+                </a>
+              </li>
+
+              <li className="flex items-center">
+                <a
+                  className={
+                    (props.transparent
+                      ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                      : "text-gray-800 hover:text-gray-600") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href="#pablo"
+                >
+                  <i
+                    className={
+                      (props.transparent
+                        ? "lg:text-gray-300 text-gray-500"
+                        : "text-gray-500") +
+                      " fab fa-github text-lg leading-lg "
+                    }
+                  />
+                  <span className="lg:hidden inline-block ml-2">Star</span>
+                </a>
+              </li>
+
+              <li className="flex items-center">
+                <button
+                  className={
+                    (props.transparent
+                      ? "bg-white text-gray-800 active:bg-gray-100"
+                      : "bg-pink-500 text-white active:bg-pink-600") +
+                    " text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                  }
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  <i className="fas fa-arrow-alt-circle-down"></i> Download
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
