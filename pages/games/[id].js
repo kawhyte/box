5c2907790cbdf6bf881bb59da9b3d56b721dc629
Kenny Whyte <kawhyte@gmail.com>
2021-03-05 19:31:53 -0800

    details section

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -61,7 +61,7 @@ export const getStaticProps = async (context) => {
 const Details = ({ data }) => {
 	console.log("New Test______", data);
 	console.log("New URLLL", data[0].url);
-
+	let factor = 3.5;
 	let color = "";
 	let skip = "hidden";
 	let must_play = "hidden";
@@ -142,7 +142,9 @@ const Details = ({ data }) => {
 									<p class='mt-2  text-base text-center md:text-left md:text-base'>
 										Released on {data[0].first_release_date}
 									</p>
-									<p class='mt-2  text-base text-center md:text-left md:text-base'>Genre: {data[0].genres[0].name}</p>
+									<p class='mt-2  text-base text-center md:text-left md:text-base'>
+										Genre: {data[0].genres[0].name}
+									</p>
 
 									<div class='mt-6 flex flex-col justify-center px-4 py-2 bg-white border border-gray-300 rounded'>
 										<div>
@@ -157,7 +159,7 @@ const Details = ({ data }) => {
 												%
 											</p>
 
-											<div className='flex items-center justify-center md:justify-start leading-tight mt-2 '>
+											<div className='flex items-center justify-center  leading-tight mt-2 '>
 												<span
 													className={
 														"py-2 px-2 items-center mr-2 flex flex-col  " +
@@ -192,7 +194,7 @@ const Details = ({ data }) => {
 														className={
 															"px-2 text-xs mt-2 text-gray-100  " + outstanding
 														}>
-														Outstanding
+														Gold
 													</p>
 												</span>
 
@@ -247,213 +249,112 @@ const Details = ({ data }) => {
 					</div>
 				</section>
 
-				<section className='relative py-0  container  '>
-					<div className=' mx-auto px-4 bg-lightgray '>
-						<div className=' grid  gap-1 grid-cols-1 lg:grid-cols-3  '>
-							<div className='w-full  ml-auto mr-auto px-4 col-span-2 '>
-								<div className='w-full max-w-3xl  mr-auto ml-auto'>
-									<div className='relative mt-8 flex flex-col min-w-0 break-words bg-white w-full mb-4  '>
-										<div className='w-full align-middle rounded-t-lg'>
-											<h4 className='text-xl text-textwhite font-bold relative py-4  '>
-												{data[0].name} Trailer
-											</h4>
-											<ReactPlayer
-												url={`https://www.youtube.com/watch?v=${data[0].videos[0].video_id}`}
-												width='640px'
-												height='360px'
-												controls
-												volume={0}
-												muted
-											/>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className='w-full  px-4'>
-								<div className='md:pr-12 '>
-									<section className='pt-3 lg:my-8  '>
-										<h3 className='text-xl font-semibold'>Genre</h3>
-										<ul className='flex flex-wrap items-center mt-1 mb-2'>
-											{data[0].genres.map((genre, i) => {
-												return (
-													<li key={i}>
-														{" "}
-														<span
-															target='_blank'
-															rel='noopener noreferrer'
-															className='text-xs font-semibold inline-block py-1 px-2  rounded text-white  bg-gray-700 uppercase last: m-1 mr-1'>
-															{" "}
-															{genre.name}
-														</span>{" "}
-													</li>
-												);
-											})}
-										</ul>
-									</section>
-									<section className=' '>
-										<h3 className='text-xl font-semibold my-2'>Platform</h3>
-										<ul className='flex flex-wrap items-center mt-1 mb-2'>
-											{data[0].platforms.map((platform, i) => {
-												return (
-													<span
-														key={i}
-														target='_blank'
-														rel='noopener noreferrer'
-														className='text-xs font-semibold inline-block py-1 px-2  rounded  bg-blue-100 uppercase last: m-1  mr-1'>
-														{platform.name}
-													</span>
-												);
-											})}
-										</ul>
-									</section>
-								</div>
-							</div>
-						</div>
+				<section className='container bg-lightgray flex flex-col justify-center items-center '>
+					<div className=' flex flex-col align-middle justify-center items-center rounded-t-lg '>
+						<h4 className='text-xl text-textwhite font-bold  py-4 text-center  '>
+							{data[0].name} Trailer
+						</h4>
+
+						<ReactPlayer
+							className=' bg-red-200'
+							url={`https://www.youtube.com/watch?v=${data[0].videos[0].video_id}`}
+							width={`${factor * 16}vw`}
+							height={`${factor * 9}vw`}
+							controls
+							volume={0}
+							muted
+						/>
 					</div>
+
+					<div className='w-full  px-4 flex flex-row '>
+						<section className='pt-3 lg:my-8  '></section>
+						<section className=' pt-3 lg:my-8'></section>
+					</div>
+
+					
 				</section>
 
 				<section className='pt-20 pb-48'>
 					<div className='container mx-auto px-4'>
-						<div className='flex flex-wrap justify-center text-center mb-24'>
+						{data[0].storyline ? <div className='flex flex-wrap justify-center text-center mb-24'>
 							<div className='w-full lg:w-11/12 px-4'>
 								<h2 className='text-xl font-semibold'>STORYLINE</h2>
 								<p className='text-lg leading-relaxed m-4 text-gray-600'>
 									{data[0].storyline}
 								</p>
 							</div>
-						</div>
+						</div>: ""}
 						<div className='flex flex-wrap'>
 							<div className='w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4'>
-								<div className='px-6'>
-									<img
-										alt='...'
-										src='/team-1-800x800.jpg'
-										className='shadow-lg rounded-full max-w-full mx-auto'
-										style={{ maxWidth: "120px" }}
-									/>
-									<div className='pt-6 text-center'>
-										<h5 className='text-xl font-bold'>Ryan Tompson</h5>
-										<p className='mt-1 text-sm text-gray-500 uppercase font-semibold'>
-											Web Developer
-										</p>
-										<div className='mt-6'>
-											<button
-												className='bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-twitter'></i>
-											</button>
-											<button
-												className='bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-facebook-f'></i>
-											</button>
-											<button
-												className='bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-dribbble'></i>
-											</button>
-										</div>
-									</div>
-								</div>
+							<h3 className='text-xl font-semibold'>Genre</h3>
+                            <ul className='flex flex-wrap items-center mt-1 mb-2'>
+                                {data[0].genres.map((genre, i) => {
+                                    return (
+                                        <li key={i}>
+                                            {" "}
+                                            <span
+                                                target='_blank'
+                                                rel='noopener noreferrer'
+                                                className='text-xs font-semibold inline-block py-1 px-2  rounded text-white  bg-gray-700 uppercase last: m-1 mr-1'>
+                                                {" "}
+                                                {genre.name}
+                                            </span>{" "}
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+
 							</div>
 							<div className='w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4'>
-								<div className='px-6'>
-									<img
-										alt='...'
-										src='/team-2-800x800.jpg'
-										className='shadow-lg rounded-full max-w-full mx-auto'
-										style={{ maxWidth: "120px" }}
-									/>
-									<div className='pt-6 text-center'>
-										<h5 className='text-xl font-bold'>Romina Hadid</h5>
-										<p className='mt-1 text-sm text-gray-500 uppercase font-semibold'>
-											Marketing Specialist
-										</p>
-										<div className='mt-6'>
-											<button
-												className='bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-google'></i>
-											</button>
-											<button
-												className='bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-facebook-f'></i>
-											</button>
-										</div>
-									</div>
-								</div>
+							<h3 className='text-xl font-semibold my-2'>Platform</h3>
+                            <ul className='flex flex-wrap items-center mt-1 mb-2'>
+                                {data[0].platforms.map((platform, i) => {
+                                    return (
+                                        <span
+                                            key={i}
+                                            target='_blank'
+                                            rel='noopener noreferrer'
+                                            className='text-xs font-semibold inline-block py-1 px-2  rounded  bg-blue-100 uppercase last: m-1  mr-1'>
+                                            {platform.name}
+                                        </span>
+                                    );
+                                })}
+                            </ul>
+
 							</div>
 							<div className='w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4'>
-								<div className='px-6'>
-									<img
-										alt='...'
-										src='/team-3-800x800.jpg'
-										className='shadow-lg rounded-full max-w-full mx-auto'
-										style={{ maxWidth: "120px" }}
-									/>
-									<div className='pt-6 text-center'>
-										<h5 className='text-xl font-bold'>Alexa Smith</h5>
-										<p className='mt-1 text-sm text-gray-500 uppercase font-semibold'>
-											UI/UX Designer
-										</p>
-										<div className='mt-6'>
-											<button
-												className='bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-google'></i>
-											</button>
-											<button
-												className='bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-twitter'></i>
-											</button>
-											<button
-												className='bg-gray-800 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-instagram'></i>
-											</button>
-										</div>
-									</div>
-								</div>
+							<h3 className='text-xl font-semibold my-2'>Platform</h3>
+                            <ul className='flex flex-wrap items-center mt-1 mb-2'>
+                                {data[0].platforms.map((platform, i) => {
+                                    return (
+                                        <span
+                                            key={i}
+                                            target='_blank'
+                                            rel='noopener noreferrer'
+                                            className='text-xs font-semibold inline-block py-1 px-2  rounded  bg-blue-100 uppercase last: m-1  mr-1'>
+                                            {platform.name}
+                                        </span>
+                                    );
+                                })}
+                            </ul>
+
 							</div>
 							<div className='w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4'>
-								<div className='px-6'>
-									<img
-										alt='...'
-										src='/team-4-470x470.png'
-										className='shadow-lg rounded-full max-w-full mx-auto'
-										style={{ maxWidth: "120px" }}
-									/>
-									<div className='pt-6 text-center'>
-										<h5 className='text-xl font-bold'>Jenna Kardi</h5>
-										<p className='mt-1 text-sm text-gray-500 uppercase font-semibold'>
-											Founder and CEO
-										</p>
-										<div className='mt-6'>
-											<button
-												className='bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-dribbble'></i>
-											</button>
-											<button
-												className='bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-google'></i>
-											</button>
-											<button
-												className='bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-twitter'></i>
-											</button>
-											<button
-												className='bg-gray-800 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1'
-												type='button'>
-												<i className='fab fa-instagram'></i>
-											</button>
-										</div>
-									</div>
-								</div>
+							<h3 className='text-xl font-semibold my-2'>Platform</h3>
+                            <ul className='flex flex-wrap items-center mt-1 mb-2'>
+                                {data[0].platforms.map((platform, i) => {
+                                    return (
+                                        <span
+                                            key={i}
+                                            target='_blank'
+                                            rel='noopener noreferrer'
+                                            className='text-xs font-semibold inline-block py-1 px-2  rounded  bg-blue-100 uppercase last: m-1  mr-1'>
+                                            {platform.name}
+                                        </span>
+                                    );
+                                })}
+                            </ul>
+
 							</div>
 						</div>
 					</div>
