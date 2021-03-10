138ee04c1fe6e879ff62e229ff89e8268bd55295
Kenny Whyte <kawhyte@gmail.com>
2021-03-10 09:48:17 -0800

    .

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -14,7 +14,7 @@ export const getStaticPaths = async () => {
 			Authorization: process.env.Authorization,
 		},
 		data:
-			"fields age_ratings,artworks.*,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover.*,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres.*,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots.*,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos.*,websites;limit 50; where id = (11169,109462,26192,113112,75235);",
+			"fields age_ratings,artworks.*,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover.*,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres.*,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots.*,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos.*,websites;limit 50; where id = (26192,109462,113112,75235,11169, 133004, 1877, 126098, 134581, 119295);",
 	})
 		.then((response) => {
 			return response.data;
