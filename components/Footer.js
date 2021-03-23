a4be8279867ce40a8e2ad2b926954595504d670d
Kenny Whyte <kawhyte@gmail.com>
2021-03-22 20:13:53 -0700

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,32 +9,10 @@ function Footer() {
 
 				<div className='container mx-auto px-4 lg:pt-6 lg:pb-2'>
 					<div className='flex flex-wrap justify-center'>
+					
+				
 						<div className='w-full lg:w-3/12 px-4 text-center'>
-							<div className='text-gray-100 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-								<i className='fas fa-medal text-xl'></i>
-							</div>
-							<h6 className='text-xl mt-5 font-semibold text-white'>GameBox</h6>
-							<p className='mt-2 mb-4 text-gray-500'>
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-						</div>
-						<div className='w-full lg:w-3/12 px-4 text-center'>
-							<div className='text-gray-100 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-								<i className='fas fa-poll text-xl'></i>
-							</div>
-							<h5 className='text-xl mt-5 font-semibold text-white'>
-								Grow your market
-							</h5>
-							<p className='mt-2 mb-4 text-gray-500'>
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-						</div>
-						<div className='w-full lg:w-3/12 px-4 text-center'>
-							<div className='text-gray-100 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-								<i className='fas fa-lightbulb text-xl'></i>
-							</div>
+						
 							<h5 className='text-lg mt-5 font-semibold text-white'>
 								Powered by
 							</h5>
