1c7431b089a4c68c2428e0a7a0a28ab9e7e3d0c9
Kenny Whyte <kawhyte@gmail.com>
2021-05-25 16:22:24 -0700

    test

--- pages/game-finder.tsx
+++ pages/game-finder.tsx
@@ -59,86 +59,78 @@ const nintendo: React.FC<IGameFull> = ({ games, startCountAt }) => {
 				paragraphText={"Not sure which game to play next? We can help you out."}
 			/>
 
-			<ul className='md:grid md:grid-cols-2 md:col-gap-10 md:row-gap-10'>
-				<li>
-					<div className='flex mb-5 mt-6'>
-						<div className='flex-shrink-0'>
-							<div className='flex items-center justify-center  text-white'>
-								<svg
-									className=' fill-current text-green-600 inline-block h-6 w-6'
-									xmlns='http://www.w3.org/2000/svg'
-									width='24'
-									height='24'
-									viewBox='0 0 24 24'>
-									<path d='M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z' />
-								</svg>
-							</div>
+			<section className='text-gray-600 body-font'>
+				<div className='container px-5 py-24 mx-auto flex flex-wrap flex-col'>
+					<div className='flex mx-auto flex-wrap mb-20'>
+						<div className='sm:px-3 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium  inline-flex items-center leading-none border-yellow-500  tracking-wider rounded-t'>
+							Enter Video a Game Name
 						</div>
-						<div className='ml-4'>
-							<h5 className='text-lg leading-6 font-medium text-gray-500 md:text-xl'>
-								Enter Video Game name
-							</h5>
+						<div className='sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-yellow-500  tracking-wider'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								className='h-8 w-8 mr-3 text-gray-100'
+								fill='none'
+								viewBox='0 0 24 24'
+								stroke='currentColor'>
+								<path
+									stroke-linecap='round'
+									stroke-linejoin='round'
+									stroke-width='2'
+									d='M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z'
+								/>
+							</svg>
+							Click the "FIND GAMES"
 						</div>
+						<a className='sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-yellow-500   tracking-wider'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								className='h-8 w-8 mr-3 text-gray-100'
+								fill='none'
+								viewBox='0 0 24 24'
+								stroke='currentColor'>
+								<path
+									stroke-linecap='round'
+									stroke-linejoin='round'
+									stroke-width='2'
+									d='M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z'
+								/>
+							</svg>
+							Discover great video games
+						</a>
 					</div>
-					<div className='flex mb-5'>
-						<div className='flex-shrink-0'>
-							<div className='flex items-center justify-center  text-white'>
-								<svg
-									className=' fill-current text-green-600  inline-block h-6 w-6'
-									xmlns='http://www.w3.org/2000/svg'
-									width='24'
-									height='24'
-									viewBox='0 0 24 24'>
-									<path d='M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z' />
-								</svg>
-							</div>
-						</div>
-						<div className='ml-4'>
-							<h5 className='text-lg leading-6 font-medium text-gray-500 md:text-xl'>
-								Click the "FIND GAMES"
-							</h5>
-						</div>
-					</div>
-					<div className='flex mb-5'>
-						<div className='flex-shrink-0'>
-							<div className='flex items-center justify-center  text-white'>
-								<svg
-									className=' fill-current text-green-600  inline-block h-6 w-6'
-									xmlns='http://www.w3.org/2000/svg'
-									width='24'
-									height='24'
-									viewBox='0 0 24 24'>
-									<path d='M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.393 7.5l-5.643 5.784-2.644-2.506-1.856 1.858 4.5 4.364 7.5-7.643-1.857-1.857z' />
-								</svg>
-							</div>
-						</div>
-						<div className='ml-4'>
-							<h5 className='text-lg leading-6 font-medium text-gray-500 md:text-xl'>
-								Discover great video games
-							</h5>
-						</div>
-					</div>
-				</li>
-			</ul>
 
-			<form onSubmit={registerUser}
-			className='mb-0 mt-4 sm:mt-4 flex justify-center '>
-				<label htmlFor='name'></label>
-				<input
-				className='w-2/3 rounded-l-lg p-3 border-t mr-0 border-b border-l text-gray-800 border-gray-400 bg-white'
-				placeholder=' Enter a video game  & get some awesome suggestions EX. Mario Maker'
-				
-				id='name' name='name' type='text' autoComplete='name' required />
-				<button className='text-gray-100 bg-green-600 py-1 px-2 rounded-r-lg  hover:bg-green-800  font-bold uppercase border-purple-500 ' type='submit'>Find Games</button>
-			</form>
-		
+					<form
+						onSubmit={registerUser}
+						className='mb-0 mt-2 sm:mt-2 flex justify-center mx-64 '>
+						<label htmlFor='name'></label>
+						<input
+							className='w-2/3 rounded-l-lg p-3 border-t mr-0 border-b border-l text-gray-800 border-gray-400 bg-white'
+							placeholder='EX. Mario Maker'
+							id='name'
+							name='name'
+							type='text'
+							autoComplete='name'
+							required
+						/>
+						<button
+							className='text-gray-100 bg-green-600 py-1 px-2 rounded-r-lg  hover:bg-green-800  font-bold uppercase border-purple-500 '
+							type='submit'>
+							Find Games
+						</button>
+					</form>
+
+					<div className='flex flex-col text-center w-full'>
+						<p className='lg:w-2/3 mx-auto my-8 leading-relaxed text-base'>
+							Enter a video game you love & get some awesome suggestions.
+						</p>
+					</div>
+				</div>
+			</section>
 
 			<Card
 				games={sortedGames}
 				startCountAt={1}
-				headerText={
-					"Check out the top 50 best video games, according to players:"
-				}
+				headerText={"Top game suggestions:"}
 			/>
 		</div>
 	);
