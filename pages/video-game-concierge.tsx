8737852109dda6fbc69c2424070a2326f8af6ac6
Kenny Whyte <kawhyte@gmail.com>
2021-08-03 20:58:28 -0700

    .

--- pages/video-game-concierge.tsx
+++ pages/video-game-concierge.tsx
@@ -38,14 +38,36 @@ const best_games = ({ games }) => {
 	const { data: snippets, mutate } = useSWR("api/gamedata");
 
 	return (
-		<UserProvider>
+	
 		<div className=''>
 			<Head>
 				<title>GameBox | Video Game Bucket List</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<SectionHero headerText={"The BEST VIDEO GAMES OF THE DECADE"} paragraphText = {"Video games you should play before you die."} /> 
+			
+
+
+			<section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+        <br className="hidden lg:inline-block" />readymade gluten
+      </h1>
+      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">2021</button>
+        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">2020</button>
+      </div>
+    </div>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+    </div>
+  </div>
+</section>
+
+
+
 
 			 <SectionHeaderText
 				headerText={" Top 100 Video Game Bucket List"}
@@ -54,14 +76,12 @@ const best_games = ({ games }) => {
 				}
 			/> 
 
-			{/*snippets &&
-				snippets.map((snippet) => (
-					<Snippet key={snippet.id} snippet={snippet} />
-				))*/}
+		
 
 			<GameList todos={todos} toggleTodo={toggleTodo} />
 		</div>
-		</UserProvider>
+		
+		
 	);
 };
 
