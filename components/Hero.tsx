84db3cd115f11eae4ddb1e1d012b14c9c14b0f7b
Kenny Whyte <kawhyte@gmail.com>
2022-01-08 15:36:44 -0800

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,8 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Hero: React.FC<IGameFull> = ({ games }) => {
-
-	console.log("Games ",games)
+	console.log("Games ", games);
 	const sortedGames = games
 		.sort((a, b) => {
 			return b.total_rating - a.total_rating;
@@ -26,71 +25,43 @@ const Hero: React.FC<IGameFull> = ({ games }) => {
 	console.log(" sortedGames[0] ", sortedGames[0]);
 	return (
 		<>
-			<section className='text-gray-600 body-font max-w-full '>
-				<div className=' mx-auto flex px-5 pb-24 items-center justify-center flex-col'>
+			<section className='text-gray-600 body-font  container '>
+				<div className=' mx-auto flex px-5 items-center justify-center flex-col'>
 					<img
-						className='lg:w-5/6 md:w-5/6  mb-10 object-cover object-center rounded  top-0 w-full h-full bg-center bg-cover'
+						className=' mb-10 object-cover object-center rounded  top-0 w-full h-full bg-center bg-cover'
 						alt='hero'
-						src={games[0].screenshots[0].url.replace(
-							"t_thumb",
-							"t_screenshot_big"
-						)}
+						src={games[
+							Math.floor(Math.random() * games.length)
+						].screenshots[0].url.replace("t_thumb", "t_screenshot_big")}
 					/>
-					<div className='text-center lg:w-2/3 w-full  -m-24  backdrop-blur-xl '>
-						<h1 className='py-1 px-1 text-white   font-extrabold  text-4xl sm:text-6xl  lg:text-7xl xl:text-11xl'>
-							Find Your Next Favorite
-						</h1>
+					<div className='text-center  w-full  -mt-24  flex flex-col  justify-center '>
+						<p className='py-1 px-1 text-white   font-extrabold  text-3xl sm:text-4xl whitespace-nowrap lg:text-6xl xl:text-11xl'>
+							Track games you've played.
+						</p>
+						<p className='py-1 px-1 text-white   font-extrabold  text-3xl sm:text-4xl whitespace-nowrap lg:text-6xl xl:text-11xl'>
+							Save those you want to play.
+						</p>
+						<p className='py-1 px-1 text-white   font-extrabold  text-3xl sm:text-4xl whitespace-nowrap lg:text-6xl xl:text-11xl'>
+							Tell your friends what's good.
+						</p>
 						<p className='py-2  text-indigo-500 text-lg   leading-relaxed mt-0 lg:mb-4 md:text-2xl'>
 							Personalized recommendations for Nintendo, Xbox, Playstation and
 							PC Games.
 						</p>
-						{/*<div class='flex justify-center'>
+						<div className='flex justify-center'>
 							<div className='flex align-middle items-center '>
 								<Link href='/video-game-concierge'>
 									<button className='uppercase py-2 px-4 mx-1 text-base sm:text-base tracking-wide sm:px-8 sm:py-3 rounded bg-indigo-500 hover:bg-indigo-600 font-semibold text-textwhite  shadow-sm hover:shadow-lg'>
-										View Games
+									GET STARTED — IT‘S FREE!
 									</button>
 								</Link>
 							</div>
 
-							<button class='ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg'>
-								Button
-							</button>
-						</div>*/}
+							
+						</div>
 					</div>
 				</div>
 			</section>
-
-			<div className='flex flex-col   align-middle items-center mt-16 '>
-				
-
-				<section className='cursor-pointer container mx-auto grid grid-cols-2  lg:grid-cols-4  gap-3     my-6  w-full  '>
-					<Link href='/nintendo'>
-						<div className='bg-gray-900  flex flex-col align-middle items-center px-16 py-4  mx-8  rounded-3xl hover:bg-gray-700'>
-							<Image width={96} height={96} src='/console.png' />
-							Nintendo
-						</div>
-					</Link>
-					<Link href='/xbox'>
-						<div className='bg-gray-900 flex flex-col align-middle items-center px-16 py-4  mx-8  rounded-3xl hover:bg-gray-700'>
-							<Image width={96} height={96} src='/game-console.png' />
-							Xbox
-						</div>
-					</Link>
-					<Link href='/playstation'>
-						<div className='bg-gray-900 flex flex-col align-middle items-center px-16 py-4  mx-8  rounded-3xl hover:bg-gray-700'>
-							<Image width={96} height={96} src='/playstation.png' />
-							PlayStation
-						</div>
-					</Link>
-					<Link href='/playstation'>
-						<div className='bg-gray-900 flex flex-col align-middle items-center px-16 py-4  mx-8  rounded-3xl hover:bg-gray-700'>
-							<Image width={96} height={96} src='/pc.png' />
-							PC
-						</div>
-					</Link>
-				</section>
-			</div>
 		</>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ import SectionHeaderText from "../components/SectionHeaderText";
 import IndexTrendingGames from "../components/IndexTrendingGames";
 import JustAdded from "../components/JustAdded";
 import Navbar from "../components/Navbar";
+import Link from "next/link";
+import Image from "next/image";
 
 //export const getStaticProps: GetStaticProps = getGamesByID(GamesoftheYear2020);
 export const getStaticProps: GetStaticProps = getIndexPageGamesByID(TrendingGames, GamesoftheYear2020);
@@ -42,12 +44,46 @@ const Home = ({ trendingGames, games2020 }) => {
 					</p>
 				</div>
 	</div>*/}	
+
+
+
 	
 	<div className='justify-center flex flex-col items-center  '>
 				<div className=' mb-4  flex flex-col align-middle justify-center items-center text-center font-black  text-textwhite tracking-wider  '>
 				
 					<IndexTrendingGames games={trendingGames} headerText={"TRENDING VIDEO GAMES"} subText={"Video Games to keep an eye on"} />
 				</div>
+			</div>
+
+			<div className='flex flex-col   align-middle items-center mt-16 '>
+				
+
+				<section className='cursor-pointer container mx-auto grid grid-cols-2  lg:grid-cols-4  gap-3     my-6  w-full  '>
+					<Link href='/nintendo'>
+						<div className='bg-gray-900  flex flex-col align-middle items-center px-16 py-4  mx-8  rounded-3xl hover:bg-gray-700'>
+							<Image width={96} height={96} src='/console.png' />
+							Nintendo
+						</div>
+					</Link>
+					<Link href='/xbox'>
+						<div className='bg-gray-900 flex flex-col align-middle items-center px-16 py-4  mx-8  rounded-3xl hover:bg-gray-700'>
+							<Image width={96} height={96} src='/game-console.png' />
+							Xbox
+						</div>
+					</Link>
+					<Link href='/playstation'>
+						<div className='bg-gray-900 flex flex-col align-middle items-center px-16 py-4  mx-8  rounded-3xl hover:bg-gray-700'>
+							<Image width={96} height={96} src='/playstation.png' />
+							PlayStation
+						</div>
+					</Link>
+					<Link href='/playstation'>
+						<div className='bg-gray-900 flex flex-col align-middle items-center px-16 py-4  mx-8  rounded-3xl hover:bg-gray-700'>
+							<Image width={96} height={96} src='/pc.png' />
+							PC
+						</div>
+					</Link>
+				</section>
 			</div>
 			<div className='justify-center flex flex-col items-center  '>
 				<div className=' mb-4  flex flex-col align-middle justify-center items-center text-center font-black  text-textwhite tracking-wider  '>
@@ -62,6 +98,7 @@ const Home = ({ trendingGames, games2020 }) => {
 				</div>
 			</div>
 		
+			
 
 
 {/*
