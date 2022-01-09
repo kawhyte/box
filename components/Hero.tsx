8a0012d4646c09ef2548543d2fa51179e73b13ce
Kenny Whyte <kawhyte@gmail.com>
2022-01-08 18:56:58 -0800

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 const Hero: React.FC<IGameFull> = ({ games }) => {
 	console.log("Games ", games);
+	let randomNum = Math.floor(Math.random() * games.length);
 	const sortedGames = games
 		.sort((a, b) => {
 			return b.total_rating - a.total_rating;
@@ -27,16 +28,21 @@ const Hero: React.FC<IGameFull> = ({ games }) => {
 		<>
 			<section className='text-gray-600 body-font  container max-w-6xl '>
 				<div className=' mx-auto flex px-5 items-center justify-center flex-col'>
-					<section
-						className='flex flex-col mx-auto justify-end items-center  bg-center opacity-50 bg-bgcolor container  bg-no-repeat bg-cover  '
+					<div
+						className='flex flex-col   mx-auto justify-end items-center  bg-center opacity-50 bg-bgcolor container  bg-no-repeat bg-cover  '
 						style={{
 							minHeight: "65vh",
 							backgroundImage: `linear-gradient(90deg,#14181d 0,rgba(20,24,29,.98556487) .97%,rgba(20,24,29,.9451312) 2.07833333%,rgba(20,24,29,.88300656) 3.29666667%,rgba(20,24,29,.80349854) 4.60166667%,rgba(20,24,29,.71091472) 5.96666667%,rgba(20,24,29,.60956268) 7.365%,rgba(20,24,29,.50375) 8.77166667%,rgba(20,24,29,.39778426) 10.16%,rgba(20,24,29,.29597303) 11.505%,rgba(20,24,29,.20262391) 12.78%,rgba(20,24,29,.12204446) 13.95833333%,rgba(20,24,29,.05854227) 15.01666667%,rgba(20,24,29,.01642493) 15.92833333%,rgba(20,24,29,0) 16.66666667%,rgba(20,24,29,0) 83.33333333%,rgba(20,24,29,.01642493) 84.07166667%,rgba(20,24,29,.05854227) 84.98333333%,rgba(20,24,29,.12204446) 86.04166667%,rgba(20,24,29,.20262391) 87.22%,rgba(20,24,29,.29597303) 88.495%,rgba(20,24,29,.39778426) 89.84%,rgba(20,24,29,.50375) 91.22833333%,rgba(20,24,29,.60956268) 92.635%,rgba(20,24,29,.71091472) 94.03333333%,rgba(20,24,29,.80349854) 95.39833333%,rgba(20,24,29,.88300656) 96.70333333%,rgba(20,24,29,.9451312) 97.92166667%,rgba(20,24,29,.98556487) 99.03%,#14181d),linear-gradient(0deg,#14181d 0,#14181d 21.48148148%,rgba(20,24,29,.98556487) 23.63703704%,rgba(20,24,29,.9451312) 26.1%,rgba(20,24,29,.88300656) 28.80740741%,rgba(20,24,29,.80349854) 31.70740741%,rgba(20,24,29,.71091472) 34.74074074%,rgba(20,24,29,.60956268) 37.84814815%,rgba(20,24,29,.50375) 40.97407407%,rgba(20,24,29,.39778426) 44.05925926%,rgba(20,24,29,.29597303) 47.04814815%,rgba(20,24,29,.20262391) 49.88148148%,rgba(20,24,29,.12204446) 52.5%,rgba(20,24,29,.05854227) 54.85185185%,rgba(20,24,29,.01642493) 56.87777778%,rgba(20,24,29,0) 58.51851852%), 
-						url(${games[
-							Math.floor(Math.random() * games.length)
-						].screenshots[0].url.replace("t_thumb", "t_screenshot_big")})`,
-						}}></section>
+							url(${games[randomNum].screenshots[0].url.replace(
+								"t_thumb",
+								"t_screenshot_big"
+							)})`,
+						}}></div>
 					<div className='text-center  w-full  -mt-24  flex flex-col  justify-center z-20 '>
+						<span className='text-right rotate-90  opacity-20    z-20 top-0 right-0 '>
+							{" "}
+							{games[randomNum].name}
+						</span>
 						<p className='py-1 px-1 text-white   font-extrabold  text-3xl sm:text-4xl whitespace-nowrap lg:text-5xl xl:text-11xl'>
 							Track games you've played.
 						</p>
